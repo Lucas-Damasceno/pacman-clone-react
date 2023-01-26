d1bc3232ae9de4e2e4fa719579ff2e037f72942b
Lucas D'Alves <lucas_d1@outlook.com>
2023-01-26 00:56:40 -0300

    refatorando código

--- src/components/pacman-controls/pacman-controls.tsx
+++ src/components/pacman-controls/pacman-controls.tsx
@@ -2,30 +2,29 @@ import React, { Dispatch, ReactElement, SetStateAction, useEffect } from "react"
 import { useRecoilState } from 'recoil';
 import config from "../../config/config";
 import { Tiles } from "../../enums/tiles.enum";
-import MazeState from "../../states/maze.state";
+import MazeState, { MazeStateType } from "../../states/maze.state";
 import PacmanState from "../../states/pacman.state";
 import { CharacterStateType } from "../../types/characterStateType";
 import { CharacterType } from "../../types/characterType";
 import Directions from "../../types/directions";
+import PossibleTiles from "../../types/possibleTiles";
 import MazeMap from "../maze/mazeMap";
 
 /* Refatorar depois */
 const validButtons = ['ArrowUp', 'ArrowLeft', 'ArrowDown', 'ArrowRight'] as const;
 type ValidButtons = typeof validButtons[number];
-
 type CharacterStateSetter = Dispatch<SetStateAction<CharacterStateType>>;
+type IndexObject<T extends string | number | symbol, K> = { [Key in T]: K }
 
-type IndexObject<T extends string | number | symbol, K> = {[Key in T]: K}
-
-function PacmanControls (): ReactElement {
+function PacmanControls(): ReactElement {
   const [mazeState, setMazeState] = useRecoilState(MazeState);
   const [pacmanState, setPacmanState] = useRecoilState(PacmanState);
 
   const handleKeyDown = (event: KeyboardEvent) => {
     /*Checagem se a tecla apertada é valida */
     const invalidKey = !validButtons.includes(event.key as any);
-    if(invalidKey) return;
-    
+    if (invalidKey) return;
+
     const keyPressed = event.key as ValidButtons;
 
     const keyPressedToDirection: IndexObject<ValidButtons, Directions> = {
@@ -34,37 +33,42 @@ function PacmanControls (): ReactElement {
       ArrowLeft: 'left',
       ArrowRight: 'right'
     }
-    
-    handleMovement(keyPressed, 'pacman', setPacmanState);
+
+    const direction: Directions = keyPressedToDirection[keyPressed];
+
+    handleMovement(direction, 'pacman', setPacmanState);
   }
 
   const canMove = (direction: Directions, characterIndex: number) => {
-    const topTile = mazeState[characterIndex - config.mazeColumns];
-    const bottomTile = mazeState[characterIndex + config.mazeColumns];
 
-    const validation: {[Key in Directions]: boolean} = {
-      right: mazeState[characterIndex].status !== Tiles.wall,
-      left: mazeState[characterIndex].status !== Tiles.wall,
-      up: topTile !== undefined && topTile.status !== Tiles.wall && topTile.status !== Tiles.ghostGate,
-      down: bottomTile !== undefined && bottomTile.status !== Tiles.wall && bottomTile.status !== Tiles.ghostGate,
+    const adjacentTiles: IndexObject<Directions, MazeStateType> = {
+      up: mazeState[characterIndex - config.mazeColumns],
+      right: mazeState[characterIndex + 1],
+      down: mazeState[characterIndex + config.mazeColumns],
+      left: mazeState[characterIndex - 1]
     }
 
-    return validation[direction];
+    const blockableTiles: PossibleTiles[] = [Tiles.wall, Tiles.ghostGate];
+
+    const tileToMove = adjacentTiles[direction];
+
+    return blockableTiles.includes(tileToMove.status)
   }
+
 
   const move = (characterState: CharacterStateSetter, direction: Directions) => {
-    
+
   }
 
-  const handleMovement = (keyPressed: ValidButtons, characterType: CharacterType, characterState: CharacterStateSetter) => {
-    
-    
-    const directions: {[Key in Directions]: () => boolean} = {
-      up: () => canMove(),
-      right: () => canMove,
-      down:() => canMove,
-      left:() => canMove,
-    }
+  const handleMovement = (direction: Directions, characterType: CharacterType, characterState: CharacterStateSetter) => {
+
+
+    // const directions: {[Key in Directions]: () => boolean} = {
+    //   up: () => canMove(),
+    //   right: () => canMove,
+    //   down:() => canMove,
+    //   left:() => canMove,
+    // }
   }
 
   useEffect(() => {
